952597d9780982e969dcc6a02d19a53f0d579c68
Jayden Robertson <jaydensouterrobertson@gmail.com>
2025-03-07 16:29:00 +0000

    Apply Menu

---
+++ commands/tools/apply.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, SelectMenuBuilder, ActionRowBuilder, SelectMenuOptionBuilder } = require("discord.js");
+
+module.exports = {
+data: new SlashCommandBuilder()
+    .setName('Apply')
+    .setDescription('Apply for verification.')
+    .addStringOption(option =>
+        option.setName('name')
+            .setDescription('Your name. Required for nickname change.')
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName('age')
+            .setDescription('Your age. Required for verification.')
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName('How did you find Astral')
+            .setDescription('How did you find Astral? If you were invited, please provide the name of the person who invited you.')
+            .setRequired(true))
+    .addStringOption(option =>
+        option.setName('Why are you interested in Astral')
+            .setDescription('What interests you about Astral?')
+            .setRequired(true)),
+};
